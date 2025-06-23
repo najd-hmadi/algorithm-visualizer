3d0e00172f6d5ff1ce7c87566681c23ff895b128
ryku <najdhmadi@gmail.com>
2025-06-23 22:46:09 +0100

    fixed some mistakes in my code

--- visualizer.cpp
+++ visualizer.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
-
+// represents blocks of memory, will probably be useful when vusalizing memory later
 struct mem_Block{
     size_t size;
     mem_Block *next;
+    bool isfree = false;
 };
 class mem_allocator{
     private:
+    // buffer represents the memory we will allocate which is a 1mb buffer if im not mistaken
     char buffer[1024*1024];
+    // making a linked list to keep track of what blocks of memory are free just to manage the buffer well
     mem_Block block_0 = {0,nullptr} ;
     public:
-    void* my_alloc(size_t size){
+    void* my_malloc(size_t size){
         mem_Block block = {0,nullptr} ;
         size_t free_mem;
         mem_Block *indexer = &block_0 ;
             while (indexer)
             {
                 
-                if (indexer->size = 0)
+                if (indexer->size == 0)
                 {
                     indexer->size = size;
                     indexer->next = &block;
@@ -24,16 +27,12 @@ class mem_allocator{
                 }
                 free_mem += indexer->size;
                 indexer = indexer->next;
-            } 
-        void *ptr = &buffer[free_mem];
+            }
+        void *ptr = &buffer + free_mem ;
         return ptr;
     }
 };
 
 int main(){
-    mem_allocator m1;
-    int *ptr = (int*)m1.my_alloc(sizeof(int));
-    *ptr = 5;
-    std::cout << "adress: " << ptr << "/// value: " << *ptr;
     return 0;
 }
